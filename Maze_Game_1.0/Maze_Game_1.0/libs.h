db43b4b9462987952b23827985317ee19d599277
Nilson Neves Filho <nilson.neves@unesp.br>
2020-09-24 01:39:44 -0300

    Maze_Game_1.0

---
+++ Maze_Game_1.0/Maze_Game_1.0/libs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<iostream>
+#include<Windows.h>
+#include<gl/GL.h>
+#include<gl/GLU.h>
+#include<glut.h>
+#include<math.h>
+#define PI 3.1415926535
+#define P2 PI/2
+#define P3 3*PI/2
+#define DR 0.0174533
+
